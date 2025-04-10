7e8f37008984fc5f9a9f518f193fd1387cffe665
nvtinh368 <71061944+nvtinh368@users.noreply.github.com>
2025-04-10 18:22:35 +0700

    Update resolver.go
    chang re

--- resolver.go
+++ resolver.go
@@ -15,9 +15,22 @@ type NameResolver interface {
 type DNSResolver struct{}
 
 func (d DNSResolver) Resolve(ctx context.Context, name string) (context.Context, net.IP, error) {
-	addr, err := net.ResolveIPAddr("ip", name)
-	if err != nil {
-		return ctx, nil, err
-	}
-	return ctx, addr.IP, err
+     r := &net.Resolver{
+        PreferGo: true,
+        Dial: func(ctx context.Context, network, address string) (net.Conn, error) {
+            d := net.Dialer{}
+            return d.DialContext(ctx, "udp", "8.8.8.8:53")
+        },
+    }
+
+    // Chỉ lấy IPv4
+    ips, err := r.LookupIP(ctx, "ip4", name)
+    if err != nil {
+        return ctx, nil, err
+    }
+    if len(ips) > 0 {
+        return ctx, ips[0], nil
+    }
+
+    return ctx, nil, net.ErrClosed
 }
